bb3937b42e232392e21f988c4139e8a77dbfce66
butlerem <butlerem01@gmail.com>
2024-11-28 15:32:55 -0500

    Initial Commit

--- utils/scanner.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def scan_network(target):
-    command = f"nmap -Pn -p 1-1024 {target}"
-    result = subprocess.run(command.split(), capture_output=True, text=True)
-    return result.stdout
